e7766f1de7344234ba2c7e74eefa523f17a8e034
smartduck66 <andre.tonic@gmail.com>
2018-03-06 21:21:07 +0100

    Commentaires rajoutés

--- TestFLTK/fltk_test.cpp
+++ TestFLTK/fltk_test.cpp
@@ -1,12 +1,9 @@
 //
-// Book : Version du chapitre 11 de l'ouvrage.
+// Book : Version du chapitre 12 de l'ouvrage.
 // "Programming -- Principles and Practice Using C++" de Bjarne Stroustrup (2ème édition : 2014)
-// Commit initial : 04/03/2018 - Installation FLTK
+// Commit initial : 04/03/2018 - Installation FLTK 1.3.4-2 (http://www.fltk.org) [conseils d'installation du site bumpyroadtocode.com car la librairie utilisée par BS, 1.1.x, était très ancienne]
 // Commit en cours : 06/03/2018 - Drill page 437
 // Caractères spéciaux : [ ]   '\n'   {  }   ||   ~   _     @
-
-// Librairie indispensable sous Windows et VC++
-// #include "stdafx.h"
 
 
 #include "Graph.h"
@@ -14,32 +11,35 @@
 
 int main()
 {
-	using namespace Graph_lib;
+	// ***********************************************************************************************************************************************************
+	// Drill de la page 437
+	
+	using namespace Graph_lib;	// Le préfixe Graph_Lib : le namespace ne semble pas fonctionner tout le temps !!! Ambiguïté avec les nouvelles librairies FLTK ?
 
 	Point tl{100, 100 };
-	Simple_window win(tl, 600, 400, "Ma première fenêtre");
+	Simple_window win(tl, 600, 400, "Ma première fenêtre");	// Inclut un bouton Next qui permet une pause
 	win.set_label("Canvas n.1");
-	win.wait_for_button();
+	//win.wait_for_button();								// On retire ces commandes pour afficher en une seule fois l'intégraliét des tracés graphiques
 
 	Axis xa{Axis::x,Point{20,300},280,10,"x axis" };
 	win.attach(xa);
 	win.set_label("Canvas n.2");
-	win.wait_for_button();
+	//win.wait_for_button();
 
 	Axis ya{ Axis::y,Point{ 20,300 },280,10,"y axis" };
 	ya.set_color(Color::cyan);
 	ya.label.set_color(Color::dark_red);
 	win.attach(ya);
 	win.set_label("Canvas n.3");
-	win.wait_for_button();
+	//win.wait_for_button();
 
 	Function sine{sin,0,100,Point{ 20,150 },1000,50,50 };
 	sine.set_color(Color::blue);
 	win.attach(sine);
 	win.set_label("Canvas n.4");
-	win.wait_for_button();
+	//win.wait_for_button();
 
-	Graph_lib::Polygon poly;		// Rajout de Graph_Lib : le namespace ne semble pas fonctionner !!! Ambiguïté avec les nouvelles librairies FLTK ?
+	Graph_lib::Polygon poly;							// Rajout de Graph_Lib 
 	poly.add(Point{ 300,200 });
 	poly.add(Point{ 350,100 });
 	poly.add(Point{ 400,200 });
@@ -47,13 +47,13 @@ int main()
 	poly.set_style(Line_style::dash);
 	win.attach(poly);
 	win.set_label("Canvas n.5");
-	win.wait_for_button();
+	//win.wait_for_button();
 	
 
-	Graph_lib::Rectangle r{ Point{ 200,200 },100,50 };
+	Graph_lib::Rectangle r{ Point{ 200,200 },100,50 };	// Rajout de Graph_Lib
 	win.attach(r);
 	win.set_label("Canvas n.6");
-	win.wait_for_button();
+	//win.wait_for_button();
 
 	Closed_polyline poly_rect;
 	poly_rect.add(Point{ 100,50 });
@@ -62,33 +62,33 @@ int main()
 	poly_rect.add(Point{ 100,100 });
 	poly_rect.add(Point{ 50,75 });
 	win.attach(poly_rect);
-	win.wait_for_button();
+	//win.wait_for_button();
 
 	r.set_fill_color(Color::yellow);
 	poly.set_style(Line_style(Line_style::dash, 4));
 	poly_rect.set_style(Line_style(Line_style::dash, 2));
 	poly_rect.set_fill_color(Color::green);
 	win.set_label("Canvas n.7");
-	win.wait_for_button();
+	//win.wait_for_button();
 
 	Text t{ Point{ 100,150 },"hello, c'est beau !" };
 	win.attach(t);
 	win.set_label("Canvas n.8");
-	win.wait_for_button();
+	//win.wait_for_button();
 
 	t.set_font(Font::times_bold);
 	t.set_font_size(20);
 	win.set_label("Canvas n.9");
-	win.wait_for_button();
+	//win.wait_for_button();
 
 	Image ii{ Point{ 100,50 },"input_choice.jpg" };
 	win.attach(ii);
 	win.set_label("Canvas n.10");
-	win.wait_for_button();
+	//win.wait_for_button();
 
 	ii.move(200,200);
 	win.set_label("Canvas n.11");
-	win.wait_for_button();
+	//win.wait_for_button();
 
 	Circle c{ Point{ 100,200 },50};
 	Graph_lib::Ellipse e{ Point{ 100,200 },75,25 };
@@ -100,6 +100,9 @@ int main()
 	win.set_label("Canvas n.12");
 	win.wait_for_button();
 
+
+	// ***********************************************************************************************************************************************************
+	// Exercices page 438 et 439
 
 }
 
